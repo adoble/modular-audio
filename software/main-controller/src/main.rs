8c1ed1a6c5868aa2da27a96581e5bca3d584bd20
Andrew Doble <andrew.l.doble@gmail.com>
2022-04-11 17:39:28 +0200

    Skeleton of callbacks to rust program

--- software/main-controller/src/main.rs
+++ software/main-controller/src/main.rs
@@ -1,13 +1,33 @@
+use std::sync::mpsc::channel;
+
+use timer::Timer;
+
 slint::include_modules!();
 
 fn main() {
     let ui = AppWindow::new();
 
-    let ui_handle = ui.as_weak();
-    ui.on_select_source(move || {
-        let _ui = ui_handle.unwrap();
-        //ui.set_counter(ui.get_counter() + 1);
+    // Set up the bluetooth timer
+    let bluetooth_timer = timer::Timer::new();
+    //let (tx, rx) = channel();
+
+
+    
+    let _ui_handle = ui.as_weak();
+    // ui.on_select_source(move || {
+    //     let _ui = ui_handle.unwrap();
+    //     ui.set_counter(ui.get_counter() + 1);
+        
+    // });
+    
+    ui.global::<Source>().on_select_source(move | id | {
+            
+     
+            println!("Selected source: {}", id);
     });
+
+
+    ui.global::<Bluetooth>().set_connecting(true);
 
     ui.run();
 }

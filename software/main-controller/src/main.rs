d20a7f3a385898aafb401377e5e10c9bb6674f75
Andrew Doble <andrew.l.doble@gmail.com>
2022-04-20 17:43:51 +0200

    Demo of timer (as BT connect simulation) works

--- software/main-controller/src/main.rs
+++ software/main-controller/src/main.rs
@@ -1,40 +1,47 @@
-use std::sync::mpsc::channel;
-
-use timer::Timer;
+use slint::{Timer, TimerMode};
 
 slint::include_modules!();
 
 fn main() {
     let ui = AppWindow::new();
 
-    // Set up the bluetooth timer
-    let bluetooth_timer = timer::Timer::new();
-    //let (tx, rx) = channel();
-
-
+    // Initialise the timer to simulate the bluetooth connection. 
+    // This has to be done outside of the callback code. 
+    let bluetooth_timer = Timer::default();
     
-    let _ui_handle = ui.as_weak();
-    // ui.on_select_source(move || {
-    //     let _ui = ui_handle.unwrap();
-    //     ui.set_counter(ui.get_counter() + 1);
-        
-    // });
-    
-    ui.global::<Source>().on_select_source(move | source_id | {
+    let ui_handle = ui.as_weak();
+
+    ui.global::<Source>().on_select_source(move |source_id| {
         println!("Selected source: {}", source_id);
+        let ui = ui_handle.unwrap();
         let selected_source = match source_id {
-            -1 | 0 => None,
+            -1 | 0 => None, 
             1..=8 => Some(source_id),
             _ => panic!("Unknown source {}", source_id),
         };
-
+       
+        
         match selected_source {
-            Some(1) => println!("Bluetooth!"),
-            _ => (),
+            Some(1) => { 
+                println!("Bluetooth!");
+                
+                let status = ui.global::<Bluetooth>().get_connecting();
+                
+                ui.global::<Bluetooth>().set_state(status);
+
+                bluetooth_timer.start(
+                    TimerMode::SingleShot,
+                    std::time::Duration::from_millis(2000),
+                    move || {
+                        ui.global::<Bluetooth>().set_state(ui.global::<Bluetooth>().get_connected());
+                        println!("Connected");
+                    },
+                );
+            },
+            //_ => (),
+            _ => println!("Another source"),
         }
-         
     });
-  
 
     ui.run();
 }
